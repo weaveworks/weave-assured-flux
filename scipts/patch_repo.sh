746b4c57a1306ed6a1158ecd3c4b85d119c2dbac
Soule BA <bah.soule@gmail.com>
2023-09-19 14:38:52 +0200

    Add a release workflow to release flux controllers
    A series of patches will be applied for each controller if it exist
    
    Signed-off-by: Soule BA <soule@weave.works>

--- scipts/patch_repo.sh
+++ scipts/patch_repo.sh
@@ -5,12 +5,17 @@ set -o errexit
 # Version: v1.0.0
 # Author: Soule BA <soule@weave.works>
 
+function p {
+    printf "\033[92mPATCH REPO => \033[96m%s\033[39m\n" "$1"
+}
+
 function clone() {
     local repo=$1
     local dir=$2
     local tag=$3
 
     if [ ! -d "$dir" ]; then
+        p "Cloning $repo into $dir with tag $tag ..."
         git clone -q --depth 1 -b "$tag" "$repo" "$dir"
     fi
 }
@@ -21,6 +26,7 @@ function patch() {
     # check that series file exists and has more than 2 lines
     lines=$(cat "$file" | wc -l)
     if [ "$lines" gt 2 ]; then
+        p "Patching repo with $file ..."
         stg init
         stg import -t --series file
         stg_version_output=$(stg --version | grep -i stacked | head -n 1)
@@ -38,7 +44,9 @@ function patch() {
 pushd . >/dev/null
 
 git clone $1 $2 $3
+p "Successfully cloned $1 into $2 with tag $3"
 cd $2
 patch ../patches-flux/$2/series
+p "Successfully patched $2"
 
 popd >/dev/null
